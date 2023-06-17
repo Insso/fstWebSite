23d38a7bd553ecd42b899c681f76a8aa632d60bd
Insso <insso.fooly@gmail.com>
2023-06-18 02:23:01 +0300

    page principal finito je débute sur le quickAdd j'ai partiellement commencer le formulaire

--- ABBOUAN-Anas_Web-project/myJS/monScript.js
+++ ABBOUAN-Anas_Web-project/myJS/monScript.js
@@ -8,3 +8,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // Définit la largeur du logo en fonction de la largeur de la sidebar
     logo.style.width = `${sidebarWidth}px`;
   });
+
+  // Supposons que tu aies un objet manga avec les informations du nouveau manga
+var manga = {
+  id: 1,
+  image: "image.png",
+  title: "Mang",
+  status: "In Progress",
+  chapter: 10,
+  progress: 5,
+  link: "https://example.com/manga"
+};
+
+
+// Sélectionne le tbody de la table
+var mangaListBody = document.getElementById("mangaListBody");
+
+// Crée une nouvelle ligne pour le manga
+var newRow = document.createElement("tr");
+
+// Ajoute les colonnes correspondantes aux informations du manga
+newRow.innerHTML = `
+  <td>${manga.id}</td>
+  <td><img src="${manga.image}" alt="${manga.title}" /></td>
+  <td>${manga.title}</td>
+  <td>${manga.status}</td>
+  <td>${manga.chapter}</td>
+  <td>${manga.progress}</td>
+  <td><a href="${manga.link}" target="_blank">Read</a></td>
+`;
+
+// Ajoute la nouvelle ligne à la liste des mangas
+mangaListBody.appendChild(newRow);
